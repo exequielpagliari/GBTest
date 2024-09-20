186851190ca3a62108bc1ba3412307612daf5ddf
Exequiel Pagliari <exepa@hotmail.com>
2024-09-20 09:01:51 -0300

    Agregado Hole. Sistema de desplazamiento por colisiones

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 #include <stdbool.h>
 #include "TileLabel.h"
 #include "box.h"
-#include <array.h>
+#include "Hole.h"
+
 
 #define PLAYER_SPRITE_START 1
+#define BOX_SPRITE_START 1
+#define BOX_VRAM_INDEX 1
+#define HOLE_VRAM_INDEX 2
 #define OAM_PLAYER 0
 
 uint8_t spriteX,spriteY;
@@ -13,7 +17,7 @@ uint8_t boxX = 20, boxY = 20;
 int8_t velocityX,velocityY;
 
 typedef struct {
-    uint8_t x, y, width, height, id;
+    uint8_t x, y, width, height, id, vram;
 } Sprite;
 
 
@@ -52,49 +56,63 @@ void UpdateInput(Sprite *Player)
 		Player->x+=velocityX;
 }
 
+void MoveColliderPlayer(Sprite sprite1, Sprite sprite2, uint8_t prevSpriteX, uint8_t prevSpriteY)
+{
+				if(sprite1.x - prevSpriteX !=0)
+				{
+					sprite2.x += sprite1.x - prevSpriteX;
+					move_sprite(sprite2.id,sprite2.x+4,sprite2.y+12);
+				}
+				if(sprite1.y - prevSpriteY !=0)
+				{
+					sprite2.y += sprite1.y - prevSpriteY;
+					move_sprite(sprite2.id,sprite2.x+4,sprite2.y+12);
+				}
+
+}
+
 void main(void)
 {
 	DISPLAY_ON;
     SHOW_SPRITES;
 
-	auto Sprite Player = {0 , 0 , 4, 4,0};
-	auto Sprite Sp1 = {20,20,4,4,1};
-	auto Sprite Sp2 = {60,120,4,4,2};
-	auto Sprite Sp3 = {40,100,4,4,3};
-	auto Sprite Sp4 = {100,20,4,4,4};
-	auto Sprite Sp5 = {130,90,4,4,5};
+	auto Sprite Player = {0 , 0 , 4, 4,0,0};
+	auto Sprite Sp1 = {20,20,4,4,1,BOX_VRAM_INDEX};
+	auto Sprite Sp2 = {60,120,4,4,2,BOX_VRAM_INDEX};
+	auto Sprite Sp3 = {40,100,4,4,3,BOX_VRAM_INDEX};
+	auto Sprite Sp4 = {100,20,4,4,4,BOX_VRAM_INDEX};
+	auto Sprite Sp5 = {130,90,4,4,5,BOX_VRAM_INDEX};
+	auto Sprite Sp6 = {35,35,4,4,6,BOX_VRAM_INDEX};
 
-
+	auto Sprite	Hl0 = {50,50,6,6,7,HOLE_VRAM_INDEX};
 	
-    set_sprite_data(1,1,box);
-	set_sprite_data(2,1,box);
-	set_sprite_data(3,1,box);
-	set_sprite_data(4,1,box);
-	set_sprite_data(5,1,box);
+    set_sprite_data(1,BOX_SPRITE_START,box);
+	set_sprite_data(2,1,Hole);
+	set_sprite_tile(7,2);
+	for(uint8_t i=1;i<7;i++)
+	set_sprite_tile(i,BOX_VRAM_INDEX);
+	
 
-
-	set_sprite_tile(1,1);
-	set_sprite_tile(2,2);
-	set_sprite_tile(3,3);
-	set_sprite_tile(4,4);
-	set_sprite_tile(5,5);
     
 	
 	
-	set_sprite_data(0,1,TileLabel);
+	set_sprite_data(0,PLAYER_SPRITE_START,TileLabel);
     set_sprite_tile(0,0);
 	
 	Player.x = 84;
 	Player.y = 88;
 	
-    move_sprite(0,Player.x,Player.y);
+    move_sprite(Player.id,Player.x,Player.y);
 
-	move_sprite(1,Sp1.x+4,Sp1.y+12);
-	move_sprite(2,Sp2.x+4,Sp2.y+12);
-	move_sprite(3,Sp3.x+4,Sp3.y+12);
-	move_sprite(4,Sp4.x+4,Sp4.y+12);
-	move_sprite(5,Sp5.x+4,Sp5.y+12);
+	move_sprite(Sp1.id,Sp1.x+4,Sp1.y+12);
+	move_sprite(Sp2.id,Sp2.x+4,Sp2.y+12);
+	move_sprite(Sp3.id,Sp3.x+4,Sp3.y+12);
+	move_sprite(Sp4.id,Sp4.x+4,Sp4.y+12);
+	move_sprite(Sp5.id,Sp5.x+4,Sp5.y+12);
+	move_sprite(Sp6.id,Sp6.x+4,Sp6.y+12);
 
+
+	move_sprite(Hl0.id,Hl0.x+4,Hl0.y+14);
     // Set our default position
 
 	Player.x = 80;
@@ -117,37 +135,118 @@ void main(void)
 		UpdateInput(&Player);
 
 
-		for(uint8_t i = 1; i<5;i++)
-			shadow_OAM[i]
+
 		if(collides(Player,Sp1))
 		{
+				//MoveColliderPlayer(Player,Sp1,prevSpriteX,prevSpriteY);
+				if(Player.x - prevSpriteX !=0)
+				{
+					Sp1.x += Player.x - prevSpriteX;
+					move_sprite(Sp1.id,Sp1.x+4,Sp1.y+12);
+				}
+				if(Player.y - prevSpriteY !=0)
+				{
+					Sp1.y += Player.y - prevSpriteY;
+					move_sprite(Sp1.id,Sp1.x+4,Sp1.y+12);
+				}
 				Player.x = prevSpriteX;
 				Player.y = prevSpriteY;
+				
 		}
 		
 		if(collides(Player,Sp2))
 		{
+				if(Player.x - prevSpriteX !=0)
+				{
+					Sp2.x += Player.x - prevSpriteX;
+					move_sprite(Sp2.id,Sp2.x+4,Sp2.y+12);
+				}
+				if(Player.y - prevSpriteY !=0)
+				{
+					Sp2.y += Player.y - prevSpriteY;
+					move_sprite(Sp2.id,Sp2.x+4,Sp2.y+12);
+				}
 				Player.x = prevSpriteX;
 				Player.y = prevSpriteY;
 		}
 		
 		if(collides(Player,Sp3))
 		{
+							if(Player.x - prevSpriteX !=0)
+				{
+					Sp3.x += Player.x - prevSpriteX;
+					move_sprite(Sp3.id,Sp3.x+4,Sp3.y+12);
+				}
+				if(Player.y - prevSpriteY !=0)
+				{
+					Sp3.y += Player.y - prevSpriteY;
+					move_sprite(Sp3.id,Sp3.x+4,Sp3.y+12);
+				}
 				Player.x = prevSpriteX;
 				Player.y = prevSpriteY;
 		}
 		
 		if(collides(Player,Sp4))
 		{
+							if(Player.x - prevSpriteX !=0)
+				{
+					Sp4.x += Player.x - prevSpriteX;
+					move_sprite(Sp4.id,Sp4.x+4,Sp4.y+12);
+				}
+				if(Player.y - prevSpriteY !=0)
+				{
+					Sp4.y += Player.y - prevSpriteY;
+					move_sprite(Sp4.id,Sp4.x+4,Sp4.y+12);
+				}
 				Player.x = prevSpriteX;
 				Player.y = prevSpriteY;
 		}
 		
 		if(collides(Player,Sp5))
 		{
+							if(Player.x - prevSpriteX !=0)
+				{
+					Sp5.x += Player.x - prevSpriteX;
+					move_sprite(Sp5.id,Sp5.x+4,Sp5.y+12);
+				}
+				if(Player.y - prevSpriteY !=0)
+				{
+					Sp5.y += Player.y - prevSpriteY;
+					move_sprite(Sp5.id,Sp5.x+4,Sp5.y+12);
+				}
 				Player.x = prevSpriteX;
 				Player.y = prevSpriteY;
 		}
+		
+				if(collides(Player,Sp6))
+		{
+							if(Player.x - prevSpriteX !=0)
+				{
+					Sp6.x += Player.x - prevSpriteX;
+					move_sprite(Sp6.id,Sp6.x+4,Sp6.y+12);
+				}
+				if(Player.y - prevSpriteY !=0)
+				{
+					Sp6.y += Player.y - prevSpriteY;
+					move_sprite(Sp6.id,Sp6.x+4,Sp6.y+12);
+				}
+				Player.x = prevSpriteX;
+				Player.y = prevSpriteY;
+		}
+		
+		if(collides(Player,Hl0))
+		{
+				Player.x = prevSpriteX;
+				Player.y = prevSpriteY;
+		}
+		/*
+		if(collides(Hl0,Sp1)
+		{
+			Sp1.x = -10;
+			Sp1.y = -10;
+			move_sprite(Sp1.id,Sp1.x+4,Sp1.y+12);
+		}
+		*/
 /*
 		if(boxX<spriteX+2 && boxX + 2> spriteX && boxY<spriteY-2 && boxY>spriteY+2)
 				{
