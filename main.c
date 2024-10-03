4f9aa3ce0fce5ee18c8164bf9c9aae4165e2220b
Exequiel Pagliari <exepa@hotmail.com>
2024-10-02 22:38:40 -0300

    Agregada colision entre Box

--- main.c
+++ main.c
@@ -15,23 +15,22 @@
 #define PLAYER_SPRITE_START 1
 #define BOX_SPRITE_START 1
 #define BOX_VRAM_INDEX 1
-#define HOLE_VRAM_INDEX 2
+#define PLATAFORM_VRAM_INDEX 2
 #define WALL_VRAM_INDEX 3
+#define DOOROPEN_VRAM_INDEX 4
+#define DOORCLOSE_VRAM_INDEX 5
 #define OAM_PLAYER 0
 
-uint8_t spriteX,spriteY;
-uint8_t boxX = 20, boxY = 20;
-int8_t velocityX,velocityY;
+int spriteX,spriteY;
+int boxX = 20, boxY = 20;
+int velocityX,velocityY;
 
 typedef struct Sprite{
-    uint8_t x, y, width, height, id, vram;
+    int x, y, width, height, lock, id, vram;
 } Sprite;
 
 typedef struct Door{
-	Sprite sprite;
-    Sprite spriteOpen;
-	Sprite spriteClose;
-	bool open;
+	struct Sprite sprite;
 } Door;
 
 typedef struct box{
@@ -49,7 +48,6 @@ typedef struct plataform{
 typedef struct SWTR{
     Sprite sprite;
 	bool complete;
-	Door *door;
 } SWTR;
 
 typedef struct KYTR{
@@ -94,6 +92,7 @@ void UpdateInput(struct Sprite *Player)
 
 void MoveColliderPlayer(struct Sprite *sprite1, struct Sprite *sprite2, int *x, int *y)
 {
+
 				if(sprite1->x - *x !=0)
 				{
 					sprite2->x += sprite1->x - *x; // Modify sprite2.x using pointer
@@ -109,22 +108,36 @@ void MoveColliderPlayer(struct Sprite *sprite1, struct Sprite *sprite2, int *x, 
 
 }
 
-void SetupLevel(struct box *to)
+void SetupLevel(struct box *to1, struct wall *to2 )
 {
-	struct box Box1 = {{20,20,8,8,1,BOX_VRAM_INDEX}};
-	struct box Box2 = {{60,120,8,8,2,BOX_VRAM_INDEX}};
-	struct box Box3 = {{40,100,8,8,3,BOX_VRAM_INDEX}};
-	struct box Box4 = {{100,20,8,8,4,BOX_VRAM_INDEX}};
-	struct box Box5 = {{130,90,8,8,5,BOX_VRAM_INDEX}};
-	struct box Box6 = {{35,35,8,8,6,BOX_VRAM_INDEX}};
+	struct box Box1 = {{20,20,8,8,0,1,BOX_VRAM_INDEX}};
+	struct box Box2 = {{60,120,8,8,0,2,BOX_VRAM_INDEX}};
+	struct box Box3 = {{40,100,8,8,0,3,BOX_VRAM_INDEX}};
+	struct box Box4 = {{100,20,8,8,0,4,BOX_VRAM_INDEX}};
+	struct box Box5 = {{130,90,8,8,0,5,BOX_VRAM_INDEX}};
+	struct box Box6 = {{35,35,8,8,0,6,BOX_VRAM_INDEX}};
 	
 
-	*to++ = Box1;
-	*to++ = Box2;
-	*to++ = Box3;
-	*to++ = Box4;
-	*to++ = Box5;
-	*to = Box6;
+	*to1++ = Box1;
+	*to1++ = Box2;
+	*to1++ = Box3;
+	*to1++ = Box4;
+	*to1++ = Box5;
+	*to1 = Box6;
+	
+	struct wall wa1 = {{100,100,8,8,0,8,WALL_VRAM_INDEX}};
+	struct wall wa2 = {{90,140,8,8,0,9,WALL_VRAM_INDEX}};
+	struct wall wa3 = {{90,60,8,8,0,10,WALL_VRAM_INDEX}};
+	struct wall wa4 = {{150,40,8,8,0,11,WALL_VRAM_INDEX}};
+	struct wall wa5 = {{0,0,8,8,0,12,WALL_VRAM_INDEX}};
+	struct wall wa6 = {{0,0,8,8,0,13,WALL_VRAM_INDEX}};
+	
+	*to2++ = wa1;
+	*to2++ = wa2;
+	*to2++ = wa3;
+	*to2++ = wa4;
+	*to2++ = wa5;
+	*to2 = wa6;
 	
 		
 }
@@ -138,33 +151,37 @@ void main(void)
 	set_sprite_data(1,BOX_SPRITE_START,Box);
 	set_sprite_data(2,1,Plataform);
 	set_sprite_data(3,1,Wall);
-	set_sprite_data(4,1,DoorOpen);
-	set_sprite_data(5,1,DoorClose);
-	set_sprite_tile(7,2);
-	//set_sprite_tile(8,3);
-	//set_sprite_tile(2,4);
-	for(uint8_t i=1;i<7;i++)
+	set_sprite_data(4,1,DoorClose);
+	set_sprite_data(5,1,DoorOpen);
+		for(uint8_t i=1;i<7;i++)
 	set_sprite_tile(i,BOX_VRAM_INDEX);
-	for(uint8_t i=8;i<17;i++)
+	set_sprite_tile(7,2);
+	for(uint8_t i=8;i<14;i++)
 	set_sprite_tile(i,WALL_VRAM_INDEX);
-	struct Sprite wa1 = {100,100,8,8,8,WALL_VRAM_INDEX};
-	struct Sprite wa2 = {90,140,8,8,9,WALL_VRAM_INDEX};
-	struct Sprite wa3 = {90,60,8,8,10,WALL_VRAM_INDEX};
-	struct Sprite wa4 = {150,40,8,8,11,WALL_VRAM_INDEX};
-	move_sprite(wa1.id,wa1.x,wa1.y);
-	move_sprite(wa2.id,wa2.x,wa2.y);
-	move_sprite(wa3.id,wa3.x,wa3.y);
-	move_sprite(wa4.id,wa4.x,wa4.y);
+	set_sprite_tile(15,DOOROPEN_VRAM_INDEX);
+	set_sprite_tile(16,DOORCLOSE_VRAM_INDEX);
+
+
+
+
+
 
 	//config ObjectsInitial
-	struct Sprite Player = {0 , 0 , 8, 8,0,0};
-	struct plataform plat = {{50,50,8,8,7,HOLE_VRAM_INDEX}};
+	struct Sprite Player = {0 , 0 , 8, 8,0,0,0};
+	struct plataform plat = {{50,50,8,8,0,7,PLATAFORM_VRAM_INDEX}};
 	struct box Box[6];
-	//struct Sprite Sp[6];
-	SetupLevel(Box);
+	struct wall Wal[6];
+	//struct Door door1 = {{0,0,8,8,15,DOORCLOSE_VRAM_INDEX},DOORCLOSE_VRAM_INDEX, DOOROPEN_VRAM_INDEX, 0};
+	struct Door do1 = {{20,100,8,8,1,15,DOOROPEN_VRAM_INDEX}};
+	move_sprite(do1.sprite.id,do1.sprite.x+4,do1.sprite.y+12);
+
+	SetupLevel(Box,Wal);
 	for(uint8_t i=0;i<6;i++)
 		move_sprite(Box[i].sprite.id,Box[i].sprite.x+4,Box[i].sprite.y+12);
-    // Set our default velocity to be moving down and to the right
+	for(uint8_t i=0;i<6;i++)
+		move_sprite(Wal[i].sprite.id,Wal[i].sprite.x+4,Wal[i].sprite.y+12);
+    
+	// Set our default velocity to be moving down and to the right
     velocityX=1;
     velocityY=1;
 
@@ -192,10 +209,28 @@ void main(void)
 
 
 		for(uint8_t i=0;i<6;i++)
+		{
+			int oldXposc = Box[i].sprite.x;
+			int oldYposc = Box[i].sprite.y;
 		if(collides(Player,Box[i].sprite))
 		{
-				MoveColliderPlayer(&Player,&Box[i].sprite,&prevSpriteX,&prevSpriteY);
+			MoveColliderPlayer(&Player,&Box[i].sprite,&prevSpriteX,&prevSpriteY);
 		}
+		for(uint8_t j=0;j<6;j++)
+		{
+		if(collides(Box[i].sprite,Box[j].sprite))
+		{
+			if(i == j)
+				continue;
+			Box[i].sprite.x = oldXposc;
+			Box[i].sprite.y = oldYposc;
+			Player.x = prevSpriteX;
+			Player.y = prevSpriteY;
+		}
+		}
+		}
+		
+
 		
 		if(collides(Player,plat.sprite))
 		{
@@ -203,16 +238,32 @@ void main(void)
 				Player.y = prevSpriteY;
 		}
 		
+
+		
+		
+		
 		
 		for(uint8_t i=0;i<6;i++)
 		if(collides(plat.sprite,Box[i].sprite))
 		{
-			Box[i].sprite.x = -10;
-			Box[i].sprite.y = -10;
+			Box[i].sprite.x = -10 * i;
+			Box[i].sprite.y = -10 * i;
 			move_sprite(Box[i].sprite.id,Box[i].sprite.x+4,Box[i].sprite.y+12);
 		}
 
+		if(collides(Player,do1.sprite))
+		{
+			
 
+				if(do1.sprite.lock > 0)
+				{
+				move_sprite(do1.sprite.id,0,0);
+				move_sprite(do1.sprite.id+1,do1.sprite.x+4,do1.sprite.y+12);
+				do1.sprite.lock = 0;
+				}
+
+			
+		}
 		// When we get too far to the right while moving to the right
         if((Player.x>156)){
 
